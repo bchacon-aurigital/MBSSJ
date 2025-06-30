198c14adf3502b4a2b2fa12e19a3a0cd6833fedc
bchacon-aurigital <bchacon@mainlydigitalworks.com>
2025-06-30 09:18:03 -0600

    Traduccion

--- app/components/Torneos.jsx
+++ app/components/Torneos.jsx
@@ -11,7 +11,7 @@ const Torneos = () => {
         {t('torneos.sectionTitle')}
       </div>
       <div className="grid grid-cols-1 xl:grid-cols-2">
-        <div className="md:col-span-1 flex flex-col justify-center border-[#7D7D7D]/50 border-t border-l-0 border-b border-r-0 py-14 order-last xl:pl-6">
+        <div className="md:col-span-1 flex flex-col justify-center border-[#7D7D7D]/50 border-t border-l-0 border-b border-r-0 py-14 order-last xl:pl-8">
           <h1 className="text-3xl md:text-6xl font-impact font-bold text-[#F80000] mb-4" dangerouslySetInnerHTML={{ __html: t('torneos.title') }}>
           </h1>
           <p className="mb-6 text-base md:text-xl text-[#7D7D7D] max-w-xl font-sofia font-semibold">
